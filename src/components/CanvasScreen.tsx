e7a7eff7668b140a0a524db76bc38f28fc80fba6
yuto ichinose <yuto.0526.sano@gmail.com>
2023-02-25 16:32:33 +0900

    作り替えてstopできるようにした

--- src/components/CanvasScreen.tsx
+++ src/components/CanvasScreen.tsx
@@ -1,4 +1,11 @@
-import { Box, Button, Center, Flex, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  useBoolean,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { FC, useEffect, useRef, useState } from "react";
 import ExportModal from "./ExportModal";
 import { editItemType } from "./TimecodeEditor";
@@ -15,13 +22,21 @@ const CanvasScreen: FC<CanvasScreenProps> = ({ editItems, editItemsCount }) => {
   const [videoState, setVideoState] = useState<"init" | "canplay" | "playing">(
     "init"
   );
-  const [totalTime, setTotalTime] = useState<number>(0)
+  const [timerId, setTimerId] = useState<NodeJS.Timer>();
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [calcTime, setCalcTime] = useState<number>(0);
+  const [displayTime, setDisplayTime] = useState<number>(0);
+  const [timeCode, setTimeCode] = useState<{ start: number; end: number }[]>(
+    []
+  );
+  const [totalTime, setTotalTime] = useState<number>(0);
   const [blobUrl, setBlobUrl] = useState<string>("");
   const {
     isOpen: isExportModalOpen,
     onOpen: onExportModalOpen,
     onClose: onExportModalClose,
   } = useDisclosure();
+  const [isExport, setIsExpoort] = useBoolean(false);
   const getCanvas = (): HTMLCanvasElement => {
     const canvas: any = canvasRef.current;
 
@@ -32,9 +47,45 @@ const CanvasScreen: FC<CanvasScreenProps> = ({ editItems, editItemsCount }) => {
 
     return canvas.getContext("2d");
   };
+
+  const timer = (s: number) =>
+    new Promise<void>((resolve) => {
+      const start_time = performance.now();
+      const timerid = setInterval(() => {
+        const current_time = performance.now();
+        const diff = current_time - start_time;
+        const sec = Math.floor(diff / 1000);
+        if (sec > s) {
+          resolve();
+          clearInterval(timerid);
+        }
+      }, 40);
+    });
+
   const checkItem = () => {
     if (!editItems || editItems.length === 0 || editItemsCount === 0) return;
-    setTotalTime(editItems.reduce((prev, cuurent) => prev + cuurent.sec, 0)) // 合計時間
+    setTotalTime(editItems.reduce((prev, cuurent) => prev + cuurent.sec, 0)); // 合計時間
+    setDisplayTime(0)
+    // タイムコード
+    setTimeCode(
+      editItems.map((item, index1, arr) => {
+        return index1 === 0
+          ? { start: 0, end: item.sec }
+          : {
+              start: arr.reduce(
+                (prev, current, index2) =>
+                  index1 > index2 ? prev + current.sec : prev,
+                0
+              ),
+              end: arr.reduce(
+                (prev, current, index2) =>
+                  index1 >= index2 ? prev + current.sec : prev,
+                0
+              ),
+            };
+      })
+    );
+
     const img = new Image();
     setImageElement((prev) =>
       prev
@@ -45,75 +96,109 @@ const CanvasScreen: FC<CanvasScreenProps> = ({ editItems, editItemsCount }) => {
           })
         : [img]
     );
-    screenInit()
     setVideoState("canplay");
   };
-  const animationStart = async (isExport: boolean) => {
-    if (!imageElement || !editItems || videoState === "playing") return;
-    setVideoState("playing");
-    const ctx: CanvasRenderingContext2D = getContext();
-    const timer = (s: number) =>
-      new Promise<void>((resolve) => {
-        const start_time = performance.now()
-        const timerid = setInterval(() => {
-          const current_time = performance.now();
+
+  useEffect(() => {
+    switch (videoState) {
+      case "init":
+        break;
+      case "playing":
+        const ctx: CanvasRenderingContext2D = getContext();
+        const recorder = (() => {
+          onExportModalClose();
+          //canvasの取得
+          const canvas = getCanvas();
+          //canvasからストリームを取得
+          const stream = canvas.captureStream();
+          //ストリームからMediaRecorderを生成
+          const recorder = new MediaRecorder(stream, {
+            mimeType: "video/webm;codecs=vp9",
+          });
+          return recorder;
+        })();
+        if (isExport) {
+          //ダウンロード用のリンクを準備
+          //録画終了時に動画ファイルのダウンロードリンクを生成する処理
+          recorder.ondataavailable = function (e) {
+            const videoBlob = new Blob([e.data], { type: e.data.type });
+            const blob_Url = window.URL.createObjectURL(videoBlob);
+            setBlobUrl(blob_Url);
+          };
+          //録画開始
+          recorder.start();
+        }
+
+        const start_time = performance.now() - calcTime; // 開始時間
+        const timer = setInterval(() => {
+          const current_time = performance.now(); // 終了時間
           const diff = current_time - start_time;
+          setCalcTime(diff);
+          // const sec = Math.floor((diff / 1000) * 100) / 100; // もし秒：m秒にするなら
           const sec = Math.floor(diff / 1000);
-          if (sec > s) {
-            resolve()
-            clearInterval(timerid)
+
+          // 描画する画像を選んで描画処理
+          // 現在時刻がどの画像かを計算する
+          const index = timeCode.findIndex(
+            (item) => item.start <= sec && sec < item.end
+          );
+          console.log(sec);
+          console.log(timeCode);
+          console.log(index);
+
+          if (imageElement && index > -1) {
+            const { naturalWidth: imageWidth, naturalHeight: imageHeight } =
+              imageElement[index];
+
+            reflectImage2Canvas(
+              imageElement[index],
+              imageWidth,
+              imageHeight,
+              canvasWidth,
+              canvasHeight
+            );
           }
-        }, 40)
-      });
-    const recorder = (() => {
-      onExportModalClose();
-      //canvasの取得
-      const canvas = getCanvas();
-      //canvasからストリームを取得
-      const stream = canvas.captureStream();
-      //ストリームからMediaRecorderを生成
-      const recorder = new MediaRecorder(stream, {
-        mimeType: "video/webm;codecs=vp9",
-      });
-      return recorder;
-    })();
-    if (isExport) {
-      //ダウンロード用のリンクを準備
-      //録画終了時に動画ファイルのダウンロードリンクを生成する処理
-      recorder.ondataavailable = function (e) {
-        const videoBlob = new Blob([e.data], { type: e.data.type });
-        const blob_Url = window.URL.createObjectURL(videoBlob);
-        setBlobUrl(blob_Url);
-      };
-      //録画開始
-      recorder.start();
-    }
-    for (let index = 0; index < imageElement.length; index++) {
-      const { naturalWidth: imageWidth, naturalHeight: imageHeight } =
-        imageElement[index];
 
-      reflectImage2Canvas(
-        imageElement[index],
-        imageWidth,
-        imageHeight,
-        canvasWidth,
-        canvasHeight
-      );
+          if (displayTime < sec) setDisplayTime(sec); // 表示時間更新
+          if (sec >= totalTime) {
+            // 終了
+            clearTimeout(timer);
+            setTimerId(undefined);
+            setCalcTime(0);
+            ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+            ctx.save();
+            setVideoState("canplay");
+            if (isExport) {
+              recorder.stop();
+              onExportModalOpen();
+              setIsExpoort.off();
+            }
+          }
+        }, 40);
+        setTimerId(timer);
 
-      await timer(editItems[index].sec);
-    }
+        break;
+      case "canplay":
 
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-    ctx.save();
-    setVideoState("canplay");
-    if (isExport) {
-      recorder.stop();
-      onExportModalOpen();
+        // screenInit();
+        break;
     }
+  }, [videoState]);
+  const animationStart = () => {
+    setVideoState("playing");
+    setDisplayTime(0);
   };
   const startExport = () => {
-    animationStart(true);
+    setIsExpoort.on();
+    animationStart();
   };
+  const onStop = () => {
+    setVideoState("canplay");
+    setDisplayTime(0);
+    setCalcTime(0)
+    clearTimeout(timerId);
+    setTimerId(undefined);
+  }
 
   const getOrientation = (imageDomWidth: number, imageDomHeight: number) => {
     if (imageDomWidth > imageDomHeight) {
@@ -162,7 +247,6 @@ const CanvasScreen: FC<CanvasScreenProps> = ({ editItems, editItemsCount }) => {
       imageDomHeight * ratio
     );
     canvasDomContext.save();
-
   };
   /**
    * canvasのリサイズ
@@ -204,7 +288,6 @@ const CanvasScreen: FC<CanvasScreenProps> = ({ editItems, editItemsCount }) => {
     ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     if (editItems && editItems[0].image) {
-
       img.src = URL.createObjectURL(editItems[0].image);
       img.onload = () => {
         const { naturalWidth: imageWidth, naturalHeight: imageHeight } = img;
@@ -214,18 +297,17 @@ const CanvasScreen: FC<CanvasScreenProps> = ({ editItems, editItemsCount }) => {
           imageHeight,
           canvasWidth,
           canvasHeight
-          );
-        };
-      }
-       else {
-        ctx.fillStyle = "#000";
-        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        );
+      };
+    } else {
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     }
-  }
+  };
 
-  useEffect(()=>{
-    if (videoState !== 'playing') screenInit()
-  },[canvasWidth, canvasHeight])
+  useEffect(() => {
+    if (videoState !== "playing") screenInit();
+  }, [canvasWidth, canvasHeight]);
 
   /**
    * 最大公約数を求める
@@ -257,6 +339,7 @@ const CanvasScreen: FC<CanvasScreenProps> = ({ editItems, editItemsCount }) => {
           style={{ borderBlock: "solid", borderWidth: 1, borderColor: "#000" }}
         ></canvas>
       </Box>
+      <Box>{displayTime + "/" + totalTime}</Box>
       <Flex>
         <Button
           isDisabled={(() => {
@@ -269,13 +352,13 @@ const CanvasScreen: FC<CanvasScreenProps> = ({ editItems, editItemsCount }) => {
                 return true;
             }
           })()}
-          onClick={() => animationStart(false)}
+          onClick={animationStart}
         >
           再生
         </Button>
-        {/* <Button isDisabled={videoState !== "playing"}>
+        <Button onClick={onStop} isDisabled={videoState !== "playing"}>
           停止
-        </Button> */}
+        </Button>
         <Button isDisabled={videoState !== "canplay"} onClick={startExport}>
           エクスポート
         </Button>
