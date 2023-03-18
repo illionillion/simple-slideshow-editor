9b0dc5a4be14d6cffc2c80365b2639f15cabb447
EmuraRyota <emuryo.150327@ezweb.ne.jp>
2023-03-18 21:33:41 +0900

    #3 responsive2

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,36 @@ function App() {
   const [editItems, setEditItems] = useState<editItemType[]>();
   const [editItemsCount, setEditItemsCount] = useState<number>(0);
   const [isWide] = useMediaQuery("(min-width: 700px)");
-  useEffect(()=>{
-  console.log(isWide ? "row":"column");
-  if(isWide){
-    console.log("横並び");
-  }else{
-    console.log("縦並び");
-  }
-
-  },[isWide])
+  useEffect(() => {
+    console.log(isWide ? "row" : "column");
+    if (isWide) {
+      console.log("横並び");
+    } else {
+      console.log("縦並び");
+    }
+  }, [isWide]);
   return (
-    <Center className="App" w="100vw" h="100vh" border="1px" flexDirection={isWide ? "row":"column"}>
-      <Box flex={4} h={isWide ? "full":"auto"} w={isWide ? "auto":"full"}>
+    <Center
+      className="App"
+      w="100vw"
+      h="100vh"
+      border="1px"
+      flexDirection={isWide ? "row" : "column"}
+    >
+      <Box
+        flex={4}
+        h={isWide ? "full" : "30vh"}
+        w={isWide ? "auto" : "full"}
+        borderBottom="1px"
+      >
         <CanvasScreen editItems={editItems} editItemsCount={editItemsCount} />
       </Box>
-      <Box flex={3} h={isWide ? "full":"auto"} w={isWide ? "auto":"full"}>
+      <Box
+        flex={3}
+        h={isWide ? "full" : "600px"}
+        w={isWide ? "auto" : "full"}
+        paddingTop="10px"
+      >
         <TimecodeEditor
           editItems={editItems}
           setEditItems={setEditItems}
